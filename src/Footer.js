9232618149bc1074312940aed3ddb756cba00633
tlswltn <tlswltn0124@nate.com>
2021-03-04 13:40:24 +0900

    0304

--- src/Footer.js
+++ src/Footer.js
@@ -17,7 +17,7 @@ const Footer = () => {
   return (
     <div>
       <footer className={classes.footer}>
-        <p style={{ textAlign: "center" }}>
+        <p style={{ textAlign: "center", marginTop: 20 }}>
           220-81-99881 | (주)월급날 | 대표자:임호천 | 02-785-0642 | 서울시
           영등포구 국회대로 68길 18 (여의도동, 금영빌딩)10층 <br />ⓒ 2016 payday
           co., Ltd. All Rights Reserved.
